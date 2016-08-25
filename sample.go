b8b3764a4eddb2dbfd6b7c8317b9f1fb2e48fc80
yatinwadhawan <yatin.wadhawan@hotmail.com>
2016-08-24 17:08:06 -0700

    Update sample.go

--- sample.go
+++ sample.go
@@ -1,15 +1,9 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1348\cocoasubrtf170
-{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red26\green26\blue26;\red235\green235\blue235;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sl392
 
-\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 package main\
-\
-import "fmt"\
-\
-func main() \{\
-    fmt.Printf("hello, world\\n")\
-\}}
+package main
+
+import "fmt"
+
+func main()
+{
+    fmt.Printf("hello, world\\n")
+}
